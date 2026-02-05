4d74c372ad4fc099d99894caae984396c44a3dee
Bruno santana <bruno@ethern.com.br>
2026-02-05 04:28:42 -0300

    mod

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 'use strict';
 
+require('dotenv').config()
+
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
